5c779f0ba2dffb1068f115fb2abf71413cb6bcae
Sungmiii <christyksmile@gmail.com>
2018-11-08 20:03:57 +1300

    rubbish data js file

---
+++ data/rubbishType.js
@@ -0,0 +1,23 @@
+const rubbishType = {
+    rubbish: [
+        {
+            id: 1,
+            name: 'Rubbish',
+            description: 'Rubbish in Wellington is collected by specially-designed, one-person operated trucks. The driver of the trucks is also the collector.'
+        }
+    ],
+    recycling: [
+        {
+            id: 2,
+            name: 'Recycling',
+            description: 'The Council funds household recycling collection for residents mostly through a levy on each tonne of waste that goes into the landfills. It is also funded from rubbish bag sales and the waste minimisation levy. It is not paid for from rates.'
+        }
+    ],
+    glsss: [
+        {
+            id: 3,
+            name: 'Glass',
+            description: 'The Council funds household recycling collection for residents mostly through a levy on each tonne of waste that goes into the landfills. It is also funded from rubbish bag sales and the waste minimisation levy. It is not paid for from rates.'
+        }
+    ]
+}
