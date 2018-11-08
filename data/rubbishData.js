6a35a9ef3fcb248fbfc3a67287b1495e80a02ff5
Sungmiii <christyksmile@gmail.com>
2018-11-08 20:34:25 +1300

    set up component

---
+++ data/rubbishData.js
@@ -0,0 +1,55 @@
+const rubbishData = [
+    {
+        id: 1,
+        item: "Egg cartons",
+        type: "Recycling",
+        description: "These are items and materials that can be converted into a reusable material. They are commonly found in household garbage -including paper, cardboard, beverage and food containers, metal and glass.",
+        instruction: [
+            "1. Remove lids, wash and squash your plastic, tins and cans",
+            "2. Place all recycling materials loosely in your bins or bags."
+        ]
+    },
+    {
+        id: 2,
+        item: "Tins and cans",
+        type: "Recycling",
+        description: "These are items and materials that can be converted into a reusable material. They are commonly found in household garbage -including paper, cardboard, beverage and food containers, metal and glass.",
+        instruction: [
+            "1. Remove lids, wash and squash your plastic, tins and cans",
+            "2. Place all recycling materials loosely in your bins or bags."
+        ]
+    },
+    {
+        id: 3,
+        item: "Drink bottles",
+        type: "Recycling",
+        description: "These are items and materials that can be converted into a reusable material. They are commonly found in household garbage -including paper, cardboard, beverage and food containers, metal and glass.",
+        instruction: [
+            "1. Remove lids, wash and squash your plastic, tins and cans",
+            "2. Place all recycling materials loosely in your bins or bags."
+        ]
+    },
+    {
+        id: 4,
+        item: "Food containers",
+        type: "Recycling",
+        description: "These are items and materials that can be converted into a reusable material. They are commonly found in household garbage -including paper, cardboard, beverage and food containers, metal and glass.",
+        instruction: [
+            "1. Remove lids, wash and squash your plastic, tins and cans",
+            "2. Place all recycling materials loosely in your bins or bags."
+        ]
+    },
+    {
+        id: 5,
+        item: "Pet food tins",
+        type: "Recycling",
+        description: "These are items and materials that can be converted into a reusable material. They are commonly found in household garbage -including paper, cardboard, beverage and food containers, metal and glass.",
+        instruction: [
+            "1. Remove lids, wash and squash your plastic, tins and cans",
+            "2. Place all recycling materials loosely in your bins or bags."
+        ]
+    },
+
+]
+
+export default rubbishData
---
+++ data/rubbishType.js
@@ -0,0 +1,26 @@
+const rubbishType = {
+    Rubbish: [
+        {
+            id: 1,
+            name: 'Rubbish',
+            description: 'Rubbish in Wellington is collected by specially-designed, one-person operated trucks. The driver of the trucks is also the collector.'
+        }
+    ],
+    Recycling: [
+        {
+            id: 2,
+            name: 'Recycling',
+            description: 'The Council funds household recycling collection for residents mostly through a levy on each tonne of waste that goes into the landfills. It is also funded from rubbish bag sales and the waste minimisation levy. It is not paid for from rates.'
+        }
+    ],
+    Glass: [
+        {
+            id: 3,
+            name: 'Glass',
+            description: 'The Council funds household recycling collection for residents mostly through a levy on each tonne of waste that goes into the landfills. It is also funded from rubbish bag sales and the waste minimisation levy. It is not paid for from rates.'
+        }
+    ]
+}
+
+
+export default rubbishType 
