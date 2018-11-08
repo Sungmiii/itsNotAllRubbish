b869d816e082537de11ddfdfe1fae53efaf83a3c
James <42189301+James-Is-A-Name@users.noreply.github.com>
2018-11-09 11:14:21 +1300

    add location data interaction file

--- client/locationData.js
+++ client/locationData.js
@@ -1,6 +1,128 @@
 
 
+let locationsData = [
+    {
+        "location":"Aro Valley",
+        "info":[
+            {
+                "type":"Rubbish",
+                "frequency":"once a week",
+                "day":"Wednesday",
+                "time":"8:00",
+            },
+            {
+                "type":"Recycling",
+                "frequency":"each odd week",
+                "day":"Wednesday",
+                "time":"8:00",
+            },
+            {
+                "type":"Glass",
+                "frequency":"each even week",
+                "day":"Wednesday",
+                "time":"8:00",
+            }
+        ]
+    },
+    {
+        "location":"Newtown",
+        "info":[
+            {
+                "type":"Rubbish",
+                "frequency":"once a week",
+                "day":"Thursday",
+                "time":"8:00",
+            },
+            {
+                "type":"Recycling",
+                "frequency":"each odd week",
+                "day":"Thursday",
+                "time":"8:00",
+            },
+            {
+                "type":"Glass",
+                "frequency":"each even week",
+                "day":"Thursday",
+                "time":"8:00",
+            }
+        ]
+    },
+    {
+        "location":"Hataitai",
+        "info":[
+            {
+                "type":"Rubbish",
+                "frequency":"once a week",
+                "day":"Friday",
+                "time":"8:00",
+            },
+            {
+                "type":"Recycling",
+                "frequency":"each odd week",
+                "day":"Friday",
+                "time":"8:00",
+            },
+            {
+                "type":"Glass",
+                "frequency":"each even week",
+                "day":"Friday",
+                "time":"8:00",
+            }
+        ]
+    }
+
+]
+
+let aFormat = {
+    "location": "location",
+    "info": "info",
+    "type": "type",
+    "timeDate": "day",
+    "timeHour": "time",
+    "frequency": "frequency"
+}
+
+
+function locationPickupTimes(locationName){
+    let location = locationsData.find( (location)=> (location[aFormat.location]==locationName) );
+
+    let times = location[aFormat.info].map( (info)=> {
+        return {
+            "type": info[aFormat.type],
+            "when": (""+info[aFormat.timeDate] +" "+info[aFormat.timeHour]+" "+info[aFormat.frequency])
+        }
+    })
+
+
+    //console.log(`for ${locationName} the times are `,times)
+    return times;
+
+}
+
+function rubbishTypeTimes(typeName){
+    let times = locationsData.map( (location)=> {
+        let details = location[aFormat.info].find( (rubishType)=> {
+            return (rubishType[aFormat.type] == typeName)
+        })
+
+        if(details) {
+            details[aFormat.location] = location[aFormat.location]
+            return details
+        }
+        else{
+            return null;
+        }
+    })
+
+    if (times.filter((entry)=> (entry ==null )).length == times.length){
+        return null
+    }
+
+    //console.log(`the type ${typeName} is picked up based on`,times)
+    return times;
+}
+
 module.exports = {
     locationPickupTimes,
-    rubbishType
+    rubbishTypeTimes
 }
