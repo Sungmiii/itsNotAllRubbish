8d8b181eb1a574a4c60093899af44be2f2f3d14e
Sungmiii <christyksmile@gmail.com>
2018-11-09 14:35:24 +1300

    each item link

--- client/components/RubbishData.jsx
+++ client/components/RubbishData.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Link from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import data from '../../data/rubbishData'
 const db = require('../../data/dataGet')
@@ -7,16 +7,25 @@ const db = require('../../data/dataGet')
 class RubbishData extends React.Component {
     render() {
         let rubbishType = this.props.match.params.item
-        console.log('rubbishdata page: ', data)
+        let rubbishCategory = this.props.match.params.category
+        let singleRubbish = db.categorieItems(rubbishType).map((thing) => thing.item)
+        console.log(singleRubbish)
+        console.log("the thing is", rubbishType)
+        /* console.log('rubbishdata page: ', data)
         console.log('rubbishType', rubbishType)
 
 
         console.log(db.categorieItems('Rubbish'))
-        console.log(db.itemInfo('Nappies').description)
+        console.log(db.itemInfo('Nappies').description) */
         return (
             <div className="rubbishlist">
                 <ul>
-                    {db.categorieItems(rubbishType).map((thing) => <li key={thing.id}>{thing.item}</li>)}
+                    {db.categorieItems(rubbishType).map((thing) => {
+                        console.log(thing.item, 'thing is ', thing)
+                        return (/* (<li key={thing.id}><Link to="/">{thing.item}</Link></li>)) */
+                            <li><Link to={'/environment/' + rubbishType + '/' + rubbishType + '/' + rubbishCategory}>{thing.item}
+                            </Link></li>)
+                    })}
                 </ul>
             </div>
         )
