aca9c8e12403cd04805627ef7ac185cfd929ebb0
catherineedwards <catherine@edwards-au.com>
2018-11-09 14:51:42 +1300

    Updated content

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -4,8 +4,20 @@ class Home extends React.Component {
     render() {
         return (
             <div className="homearea">
-                <h1>This is home section</h1>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus culpa quaerat sint veniam facilis ipsum minus ipsam eos suscipit sunt aperiam id sapiente expedita odit aliquid deserunt, officiis corporis.</p>
+                <h1>Welcome to Swellington City Council's Rubbish and Recycling Guide!</h1>
+                <p>
+                    Swellington City Council strives to keep the capital of Middle Zealand clean and beautiful.
+                </p>
+                <p>
+                    But we need your help to sort your rubbish and recycling.
+                </p>
+                <p>
+                    Our guide helps you help us, and we want to hear your feedback using this feedback form! 
+                </p>
+                <p>
+                    Swellington City Council: Keeping Swellington Swell since 2018!
+                </p>
+
 
             </div>
         )
