104234f157582c20874f638021f969041055174d
James <Barry@Barry.bar>
2018-11-09 15:02:48 +1300

    add server responding

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const path = require('path')
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const server = express()
 
 server.use(express.json())
 server.use(express.static(path.join(__dirname, './public')))
 
+server.use(bodyParser.urlencoded({ extended: true }))
+
 server.post('/', (req, res) => {
-    res.send("NOOOOPE")
+    res.send("NOOOOPE to you " + req.hostname)
+    console.log(req.body)
 })
 
 module.exports = server
